ceedab39041054987bbd76ecc57c426b0720f4d8
Noha2017 <31400041+Noha2017@users.noreply.github.com>
2017-09-25 15:00:02 +1000

    NS-W04-assignment

---
+++ main.js
@@ -0,0 +1,4 @@
+$("#readMore").click(slideDown)
+function slideDown(){
+  $('.readmore').css('Maecenas quis auctor dui. Ut porta aliquam lorem, at volutpat orci commodo a. Suspendisse nunc orci, porttitor ac sapien a, rutrum porttitor odio. Phasellus faucibus tristique orci eget dictum. Proin tincidunt vehicula leo sed mattis. Nunc faucibus augue a pellentesque tristique. Proin suscipit in dui ut convallis')
+};
